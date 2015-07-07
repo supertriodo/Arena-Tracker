01f6cd2ffacd62f67a9842ebeb4df6da79031698
supertriodo <triodo@gmail.com>
2015-07-07 13:17:05 +0200

    Clean up

--- Sources/mainwindow.cpp
+++ Sources/mainwindow.cpp
@@ -1,11 +1,5 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "logloader.h"
-#include "opencv2/opencv.hpp"
-#include "opencv2/core/core.hpp"
-#include "opencv2/features2d/features2d.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/nonfree/features2d.hpp"
 #include <QtWidgets>
 
 using namespace cv;
