25b40d691e85709eafad4e806ee0156b8d2cfd21
supertriodo <triodo@gmail.com>
2015-11-19 19:33:41 +0100

    SizePolicies of draft tab adjusted.

--- Sources/mainwindow.cpp
+++ Sources/mainwindow.cpp
@@ -1843,7 +1843,7 @@ void MainWindow::completeToolButton()
 //New stats site
 //Mostrar razas
 //Eliminar arenaHandler setTheme
-//Draft split Min synergy 0
+//Tam hands list
 
 //BUGS CONOCIDOS
 //Bug log tavern brawl (No hay [Bob] ---Register al entrar a tavern brawl) (Solo falla si no hay que hacer un mazo)
