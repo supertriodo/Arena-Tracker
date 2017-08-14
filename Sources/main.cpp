1fe221e62fce2cadf07df305086db846abe6f924
supertriodo <triodo@gmail.com>
2017-08-14 13:59:19 +0200

    Change style to Fusion on all platforms.

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QSplashScreen>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setStyle(QStyleFactory::create("Fusion"));
 
     QPixmap pixmap(":/Images/splash.png");
     QSplashScreen splash(pixmap, Qt::WindowStaysOnTopHint);
