f9447987f450fe1cd1d939e4b7348045c624c1b4
supertriodo <triodo@gmail.com>
2018-05-04 10:12:38 +0200

    Update version

--- Sources/versionchecker.h
+++ Sources/versionchecker.h
@@ -4,7 +4,7 @@
 #include "utility.h"
 #include <QNetworkAccessManager>
 
-#define VERSION QString("v6.5")
+#define VERSION QString("v6.51")
 #define VERSION_URL "https://raw.githubusercontent.com/supertriodo/Arena-Tracker/master/Version/version.json"
 
 
