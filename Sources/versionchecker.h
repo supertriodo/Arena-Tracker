5250c4913db76bac435d30538087c548bc77986d
supertriodo <triodo@gmail.com>
2018-07-16 12:32:21 +0200

    Update theme versions and AT version.

--- Sources/versionchecker.h
+++ Sources/versionchecker.h
@@ -4,7 +4,7 @@
 #include "utility.h"
 #include <QNetworkAccessManager>
 
-#define VERSION QString("v6.61")
+#define VERSION QString("v6.7")
 #define VERSION_URL "https://raw.githubusercontent.com/supertriodo/Arena-Tracker/master/Version/version.json"
 
 
