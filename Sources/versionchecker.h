78cfd90c4d3f63697c9271a671853505e75f78d2
supertriodo <triodo@gmail.com>
2020-02-23 19:17:48 +0100

    Update versions and themes.

--- Sources/versionchecker.h
+++ Sources/versionchecker.h
@@ -5,7 +5,7 @@
 #include <QNetworkAccessManager>
 #include <QFutureWatcher>
 
-#define VERSION QString("v7.21")
+#define VERSION QString("v8.0")
 #define VERSION_URL "https://raw.githubusercontent.com/supertriodo/Arena-Tracker/master/Version/version.json"
 
 
