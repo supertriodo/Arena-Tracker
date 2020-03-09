441f599ab5a2ea6b6f0fa48ce2514f1893c6e61c
supertriodo <triodo@gmail.com>
2020-03-09 13:13:16 +0100

    Version

--- Sources/versionchecker.h
+++ Sources/versionchecker.h
@@ -5,7 +5,7 @@
 #include <QNetworkAccessManager>
 #include <QFutureWatcher>
 
-#define VERSION QString("v8.02")
+#define VERSION QString("v8.1")
 #define VERSION_URL "https://raw.githubusercontent.com/supertriodo/Arena-Tracker/master/Version/version.json"
 
 
