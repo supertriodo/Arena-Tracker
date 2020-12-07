c815afe7a9f6f209d13111e229d68a17852eb6a5
supertriodo <triodo@gmail.com>
2020-12-07 11:48:34 +0100

    Add files via upload

---
+++ Sources/premiumhandler.h
@@ -0,0 +1,27 @@
+#ifndef PREMIUMHANDLER_H
+#define PREMIUMHANDLER_H
+
+#include "utility.h"
+#include <QObject>
+
+class PremiumHandler : public QObject
+{
+    Q_OBJECT
+public:
+    PremiumHandler(QObject *parent);
+    ~PremiumHandler();
+
+public:
+    void unlockPremium(QString email);
+    void checkTwitchSent();
+
+public slots:
+    void checkPremium(QString username="", QString password="");
+
+signals:
+    void setPremium(bool premium);
+    void pLog(QString line);
+    void pDebug(QString line, DebugLevel debugLevel=Normal, QString file="PremiumHandler");
+};
+
+#endif // PREMIUMHANDLER_H
