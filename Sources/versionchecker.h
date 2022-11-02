17bab16afc23d013800ee6930e9ae739c5c92ad5
supertriodo <triodo@gmail.com>
2022-11-02 20:41:19 +0100

    Update version

--- Sources/versionchecker.h
+++ Sources/versionchecker.h
@@ -5,7 +5,7 @@
 #include <QNetworkAccessManager>
 #include <QFutureWatcher>
 
-#define VERSION QString("v22.10")
+#define VERSION QString("v22.11")
 #define VERSION_URL "https://raw.githubusercontent.com/supertriodo/Arena-Tracker/master/Version/version.json"
 
 
