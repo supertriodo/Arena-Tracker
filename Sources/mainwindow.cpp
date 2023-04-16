c8385f4468d077e70d3e8446685296230585f773
supertriodo <triodo@gmail.com>
2023-04-16 10:54:37 +0200

    .

--- Sources/mainwindow.cpp
+++ Sources/mainwindow.cpp
@@ -4855,7 +4855,7 @@ void MainWindow::testDelay()
 
 //Cards changes
 //|-Imagenes cartas --> testDownloadRotation() --> Sobreescribir con HearthstoneSignatureCards
-//|-Synergy / Code  --> 26.0 Patch (2023-04-03)
+//|-Synergy / Code  --> 26.0.2 Patch (2023-04-14)
 
 //Rotacion CORE
 //|-Revisar cartas github CORE
