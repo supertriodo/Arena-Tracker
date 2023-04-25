148434cee8ed2fe4080b786d6ffbab36fb4f2514
supertriodo <triodo@gmail.com>
2023-04-25 10:03:04 +0200

    .

--- Sources/versionchecker.h
+++ Sources/versionchecker.h
@@ -5,7 +5,7 @@
 #include <QNetworkAccessManager>
 #include <QFutureWatcher>
 
-#define VERSION QString("v23.04")
+#define VERSION QString("v23.04.01")
 #define VERSION_URL "https://raw.githubusercontent.com/supertriodo/Arena-Tracker/master/Version/version.json"
 
 
