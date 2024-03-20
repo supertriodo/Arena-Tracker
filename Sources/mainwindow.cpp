cf242ae76032492ad8c6a498317e62d34f437753
triodo <triodo@gmail.com>
2024-03-20 10:51:26 +0100

    Update CORE synergies.

--- Sources/mainwindow.cpp
+++ Sources/mainwindow.cpp
@@ -4934,7 +4934,7 @@ void MainWindow::testDelay()
     testHeroPortraits();
     testSynergies();
 //    testTierlists();
-    testDownloadRotation(true/*, "DEEP_"*/);//Force hearthpwn true
+//    testDownloadRotation(true/*, "DEEP_"*/);//Force hearthpwn true
 //    Utility::resizeSignatureCards();
 //    Utility::mergeHSRwithFireCards();
 
