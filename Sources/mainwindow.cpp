6449c335d7838717d3daadaf8d42d2ce78467047
triodo <triodo@gmail.com>
2024-08-09 11:09:40 +0200

    30.0.3 Patch Notes

--- Sources/mainwindow.cpp
+++ Sources/mainwindow.cpp
@@ -4943,7 +4943,7 @@ void MainWindow::testDelay()
 
 //Cards changes
 //|-Imagenes cartas --> testDownloadRotation() --> Sobreescribir con HearthstoneSignatureCards (script moveCards.sh)
-//|-Synergy / Code  --> 30.0 Patch Notes (15-07-2024)
+//|-Synergy / Code  --> 30.0.3 Patch Notes (08/08/2024)
 
 //Rotacion CORE
 //|-Revisar cartas github CORE
