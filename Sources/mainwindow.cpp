467b331bdfe8f07d94f23a79103d5c9153a33c57
triodo <triodo@gmail.com>
2024-10-29 19:55:31 +0100

    Update synergies.json to 31.0 Patch Notes

--- Sources/mainwindow.cpp
+++ Sources/mainwindow.cpp
@@ -4943,7 +4943,7 @@ void MainWindow::testDelay()
 
 //Cards changes
 //|-Imagenes cartas --> testDownloadRotation() --> Sobreescribir con HearthstoneSignatureCards (script moveCards.sh)
-//|-Synergy / Code  --> 30.4 Patch Notes (09/09/2024)
+//|-Synergy / Code  --> 31.0 Patch Notes (28/10/2024)
 
 //Rotacion CORE
 //|-Revisar cartas github CORE
