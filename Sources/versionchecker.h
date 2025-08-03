dcffd401e972517a5f0d92762340507b42c0d9de
triodo <triodo@gmail.com>
2025-08-03 17:22:40 +0200

    .

--- Sources/versionchecker.h
+++ Sources/versionchecker.h
@@ -5,7 +5,7 @@
 #include <QNetworkAccessManager>
 #include <QFutureWatcher>
 
-#define VERSION QString("v25.07.03")
+#define VERSION QString("v25.08")
 #define VERSION_URL "https://raw.githubusercontent.com/supertriodo/Arena-Tracker/master/Version/version.json"
 
 
